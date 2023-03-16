4f6064e725623f3bd2f12d1449cc2c277583823a
suzuka1621 <akaneakua0625@gmail.com>
2023-03-16 15:32:09 +0900

    User.rbのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, :last_name, :first_name, :last_name_reading, :first_name_reading, :birthday, presence: true
+
+  validates :password, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers" }
+
+  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters" }
+
+  validates :last_name_reading, :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
 end
--- db/migrate/20230315112430_devise_create_users.rb
+++ db/migrate/20230315112430_devise_create_users.rb
@@ -6,11 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :nickname, null: false
-      t.string :last_name, null: false
-      t.string :first_name, null: false
-      t.string :last_name_reading, null: false
-      t.string :first_name_reading, null: false
+      t.string :nickname, null: false, default: ""
+      t.string :last_name, null: false, default: ""
+      t.string :first_name, null: false, default: ""
+      t.string :last_name_reading, null: false, default: ""
+      t.string :first_name_reading, null: false, default: ""
       t.date :birthday, null: false
 
       ## Recoverable
