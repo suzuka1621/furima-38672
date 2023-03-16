f42b4854dcf526102b48574f435a1689c87dec09
suzuka1621 <akaneakua0625@gmail.com>
2023-03-16 23:40:17 +0900

    新規登録のテストコードの実装

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ group :development, :test do
   gem 'rspec-rails', '~> 4.0.0'
   gem 'factory_bot_rails'
   gem 'faker'
+  gem 'gimei'
 end
 
 group :development do
@@ -67,5 +68,3 @@ end
 gem 'devise'
 
 gem 'pry-rails'
-
-gem 'gimei'
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-
     transient do
       person { Gimei.name }
     end
-    first_name            { person.first }
-    last_name             { person.last }
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { "o2" + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    first_name            { person.first.kanji }
+    last_name             { person.last.kanji }
     first_name_reading    { person.first.katakana }
     last_name_reading     { person.last.katakana }
     birthday              { Faker::Date.backward }
