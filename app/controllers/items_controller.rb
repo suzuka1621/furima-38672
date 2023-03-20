e58e22d8591377bce9e39e6e8dfe12ec97c2145e
suzuka1621 <akaneakua0625@gmail.com>
2023-03-21 00:57:08 +0900

    ActiveHashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
@@ -7,11 +8,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-     Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:item, :introduction, :price, :category_id, :item_status_id, :delivery_cost_id, :prefecture_id, :delivery_day_id, :user_id, :content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item, :introduction, :price, :category_id, :item_status_id, :delivery_cost_id, :prefecture_id, :delivery_day_id, :user_id, :content, :image)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,30 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_one :order
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :item_status
   belongs_to :delivery_cost
-  belongs_to :delivery_region
+  belongs_to :prefecture
   belongs_to :delivery_day
+
+  #共通のバリデーション
+  validates :item, :introduction, :price, :category_id, :item_status_id, :delivery_cost_id, :prefecture_id, :delivery_day_id, presence: true
   
-  has_one_attached :image
+  #category_idの設定
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  #item_status_idの設定
+  validates :item_status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
+  #delivery_cost_idの設定
+  validates :delivery_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  #prefecture_idの設定
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  #delivery_day_idの設定
+  validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
