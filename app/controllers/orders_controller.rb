50eae938a506dfecde8a458343abb8a9f79e82a1
suzuka1621 <akaneakua0625@gmail.com>
2023-03-31 00:56:31 +0900

    orders_controllerの記述の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,24 +4,26 @@ class OrdersController < ApplicationController
     #unless current_user.id != item.user.id || user_signed_in?
       #redirect_to root_path
     #end
+    @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new(order_params)
     if @order_delivery.valid?
        @order_delivery.save
        redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_delivery).permit(:post_code, :prefecture_id, :municipality, :address_number, :building, :phone_number, :item_id)
-    .merge(user_id: current_user.id)
+    params.require(:order_delivery).permit(:post_code, :prefecture_id, :municipality, :address_number, :building, :phone_number)
+    .merge(user_id: current_user.id, item_id: @item.id)
   end
 
 end
