9ae2e59e94322a5bce772fa329ec830c1eb21082
suzuka1621 <akaneakua0625@gmail.com>
2023-03-31 16:13:20 +0900

    バリデーションの追加

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -3,7 +3,7 @@ class OrderDelivery
   attr_accessor :post_code, :prefecture_id, :municipality, :address_number, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :user_id 
+    validates :user_id, :item_id 
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :municipality, :address_number
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is too short"}
