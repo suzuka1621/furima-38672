2dfbfa4720721daa1b8ff61fc93ea164cc282924
suzuka1621 <akaneakua0625@gmail.com>
2023-04-01 16:09:38 +0900

    購入機能のコントローラー追記

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,6 +12,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new(order_params)
     if @order_delivery.valid?
+       pay_item      
        @order_delivery.save
        redirect_to root_path
     else
@@ -22,8 +23,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_delivery).permit(:post_code, :prefecture_id, :municipality, :address_number, :building, :phone_number)
-    .merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_delivery).permit(:post_code, :prefecture_id, :municipality, :address_number, :building,
+      :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )      
+  end
 end
