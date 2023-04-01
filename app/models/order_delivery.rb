374df522c44f1380b75e65c803fc75a04a38025f
suzuka1621 <akaneakua0625@gmail.com>
2023-04-01 16:10:50 +0900

    購入機能のモデル、ビューの実装

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -1,12 +1,14 @@
 class OrderDelivery
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :municipality, :address_number, :building, :phone_number, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :municipality, :address_number, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :user_id, :item_id 
+    validates :user_id
+    validates :item_id
+    validates :token
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :municipality, :address_number
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is too short"}
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is too short or too long"}
   end
     validates :phone_number, format: { with: /\A[0-9]+\z/, message: "is invalid. Input only number" }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
